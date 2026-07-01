c801f6a39fee0522531b2c425beb7da81905d22d
navarshabeer768 <navarshabeer768@gmail.com>
2026-07-01 13:27:17 +0300

    Updates

--- _next/static/xvGHOKUb4_BImpDA9dte-/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fbilling\u002F[id]","\u002Fcustomers\u002F[id]","\u002Fcustomers\u002F[id]\u002Fedit","\u002Forders\u002F[id]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
